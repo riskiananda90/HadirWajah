9414628a1bb93f9f2c111716ed7e576809d0d74e
Riski Ananda <risskiananda90@gmail.com>
2025-05-23 08:23:23 +0700

    Dah diperbaiki ini dag fiks

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -16,10 +16,9 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 app.use((0, cors_1.default)({
-    origin: "https://hadirwajah.my.id",
+    origin: "*",
     credentials: true,
 }));
-app.options("*", (0, cors_1.default)());
 app.use(express_1.default.json({ limit: "5mb" }));
 app.use(express_1.default.urlencoded({ extended: true, limit: "5mb" }));
 app.use("/api", pelajaran_routes_1.default);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,12 +15,11 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://hadirwajah.my.id",
+    origin: "*",
     credentials: true,
   })
 );
 
-app.options("*", cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
